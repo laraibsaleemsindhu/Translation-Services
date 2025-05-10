f0ca5708c1da2452d7843636ac9f7a4b844bfc71
laraibsaleemsindhu <laraibsaleem623@gmail.com>
2025-05-10 18:39:29 +0500

    Added README file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TranslationController;
-
+//
 //Route::middleware('auth:sanctum')->group(function () {
 //    Route::get('/translations', [TranslationController::class, 'index']);
 //    Route::post('/translations', [TranslationController::class, 'store']);
@@ -13,3 +13,6 @@ Route::post('/translations', [TranslationController::class, 'store']);
 Route::put('/translations/{id}', [TranslationController::class, 'update']);
 Route::get('/translations', [TranslationController::class, 'index']);
 Route::get('/translations/export', [TranslationController::class, 'export']);
+
+
+
